1ab40bcc94453b5db2b88f8e9df19a922f089c05
Wiktor <wiktor-wosiek@wp.pl>
2024-11-30 11:50:17 +0100

    trzecie zadanie

---
+++ Oceny.js
@@ -0,0 +1,21 @@
+class Student {
+    constructor(name, age, grades) {
+        this.name = name
+        this.age = age
+        this.grades = grades
+    }
+
+    addGrade(newGrade) {
+        this.grades = [...this.grades, newGrade]
+    }
+    getAverage() {
+        const total = this.grades.reduce((sum, grade) => sum + grade, 0);
+        return total / this.grades.length;
+      }
+}
+
+let grades = [1, 3]
+const uczen = new Student('Ismail Ahmad Kanabawi', 16, grades)
+console.log(uczen.getAverage())
+uczen.addGrade(5)
+console.log(uczen.getAverage())
